c592e9300d7ddee4094e28a1327fcd1d3c8514ba
Spencer Hanson <spencerhanson3141@gmail.com>
2017-03-16 21:57:04 -0600

    Removed test code

--- RunChallenge.py
+++ RunChallenge.py
@@ -22,25 +22,4 @@ if __name__ == "__main__":
 	queryFilter = QueryFilter(shutdown);
 	commandTerminal = CommandTerminal(terminal, queryFilter.runCommand);
 	commandTerminal.start()
-	queryFilter.printHeader();
-	'''
-	print "---";
-	reader = AddressReader("ips.txt");
-	addresses = reader.getAddresses();
-	for address in addresses:
-		print address;
-	print "---";
-	'''
-	'''test_cases = ["4.2.2.2", "brokenaddress", addresses[0]];
-	for test_case in test_cases:
-		print RDAPLookup.lookupRDAP(test_case);
-		print GeoIPLookup.lookupIP(test_case);
-		time.sleep(5);
-	'''
-	#RDAP_data = RDAPLookup.lookupRDAP("4.2.2.2");
-
-	#tree_obj = objectpath.Tree(RDAP_data);
-	#print tuple(tree_obj.execute('$..label'))
-
-
-
+	queryFilter.printHeader();	
